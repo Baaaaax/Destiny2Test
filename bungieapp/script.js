7c462dfe9b94dff4132a955db5fa9654b7039580
baaaaax <you@example.com>
2019-08-07 01:39:00 +0200

    Second Commit

--- bungieapp/script.js
+++ bungieapp/script.js
@@ -1,14 +1,3 @@
-var firstInput = document.querySelector("#firstInput");
-var secondInput = document.querySelector("#secondInput");
-var submitBtn = document.querySelector("#submitBtn");
-
-var settings = {
-  method: "GET",
-  headers: {
-    "x-api-key": "cc8fc21c337a4399b94e9e11e7d908b8"
-  }
-};
-
 function createLiAndA(name, link, teamMateBool) {
   var li = document.createElement("li");
   if (teamMateBool) {
@@ -25,99 +14,18 @@ function createLiAndA(name, link, teamMateBool) {
   li.appendChild(a);
 }
 
+function createEntryMatch(string) {
+  var entryDiv = document.createElement("div");
+  entryDiv.setAttribute("class", "match-entry");
+  document.querySelector("#matchs-listing").appendChild(entryDiv);
+
+  var p = document.createElement("p");
+  p.textContent = string;
+  entryDiv.appendChild(p);
+}
+
 ///////////////////////////////////////////////////////////////////////////
 
-function getMembershipId(name) {
-  var newName = encodeURIComponent(name);
-
-  var fetchUrl =
-    "https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/4/" +
-    newName +
-    "/";
-
-  fetch(fetchUrl, settings)
-    .then(r => {
-      return r.json();
-    })
-    .then(res => {
-      return getProfile(res.Response[0].membershipId);
-    });
-}
-
-function getProfile(id) {
-  var fetchUrl =
-    "https://www.bungie.net/Platform/Destiny2/4/Profile/" +
-    id +
-    "/?components=100";
-  fetch(fetchUrl, settings)
-    .then(r => {
-      return r.json();
-    })
-    .then(res => {
-      console.log(res);
-      console.log(res.Response.profile.data.characterIds[0]);
-      return getActivityHistory(
-        res.Response.profile.data.userInfo.membershipType,
-        res.Response.profile.data.userInfo.membershipId,
-        res.Response.profile.data.characterIds[0]
-      );
-    });
-}
-
-function getActivityHistory(membType, destinyMembershipId, characterId) {
-  var fetchUrl =
-    "https://www.bungie.net/Platform/Destiny2/" +
-    membType +
-    "/Account/" +
-    destinyMembershipId +
-    "/Character/" +
-    characterId +
-    "/Stats/Activities/?count=40&mode=32";
-
-  fetch(fetchUrl, settings)
-    .then(r => {
-      return r.json();
-    })
-    .then(res => {
-      console.log(res);
-
-      res.Response.activities.forEach(element =>
-        getPostGameCarnageReport(element.activityDetails.instanceId)
-      );
-    });
-}
-
-function getPostGameCarnageReport(activityId) {
-  var fetchUrl =
-    "https://www.bungie.net/Platform/Destiny2/Stats/PostGameCarnageReport/" +
-    activityId +
-    "/";
-
-  fetch(fetchUrl, settings)
-    .then(r => {
-      return r.json();
-    })
-    .then(res => {
-      console.log(res);
-
-      res.Response.entries.forEach(element => {
-        createLiAndA(
-          element.player.destinyUserInfo.displayName,
-          "https://www.google.it/",
-          element.standing == 0
-        );
-        //checking if is in the enemy team
-        if (element.standing == 1) {
-          compareIfPlayed(element.player.destinyUserInfo.membershipId, secId);
-        }
-      });
-    });
-}
-
-function compareIfPlayed(firstId, secondId) {
-  if (firstId == secondId) {
-    console.log("played tog");
-  }
-}
-
-submitBtn.addEventListener("click", () => getMembershipId("bax#21629"));
+submitBtn.addEventListener("click", () =>
+  getFirstMembershipId("bax#21629", "duke#23539")
+);
---
+++ bungieapp/serversidefetch.js
@@ -0,0 +1,132 @@
+var firstInput = document.querySelector("#firstInput");
+var secondInput = document.querySelector("#secondInput");
+var submitBtn = document.querySelector("#submitBtn");
+
+var settings = {
+  method: "GET",
+  headers: {
+    "x-api-key": "cc8fc21c337a4399b94e9e11e7d908b8"
+  }
+};
+
+function getFirstMembershipId(firstName, secondName) {
+  var newName = encodeURIComponent(firstName);
+
+  var fetchUrl =
+    "https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/4/" +
+    newName +
+    "/";
+
+  fetch(fetchUrl, settings)
+    .then(r => {
+      return r.json();
+    })
+    .then(res => {
+      return getSecondMembershipId(res.Response[0].membershipId, secondName);
+    });
+}
+
+function getSecondMembershipId(firstId, secondName) {
+  var newName = encodeURIComponent(secondName);
+
+  var fetchUrl =
+    "https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/4/" +
+    newName +
+    "/";
+
+  fetch(fetchUrl, settings)
+    .then(r => {
+      return r.json();
+    })
+    .then(res => {
+      return getProfile(firstId, res.Response[0].membershipId);
+    });
+}
+
+function getProfile(firstId, secId) {
+  var fetchUrl =
+    "https://www.bungie.net/Platform/Destiny2/4/Profile/" +
+    firstId +
+    "/?components=100";
+  fetch(fetchUrl, settings)
+    .then(r => {
+      return r.json();
+    })
+    .then(res => {
+      console.log(res);
+      console.log(res.Response.profile.data.characterIds[0]);
+      return getActivityHistory(
+        res.Response.profile.data.userInfo.membershipType,
+        res.Response.profile.data.userInfo.membershipId,
+        res.Response.profile.data.characterIds[0],
+        secId
+      );
+    });
+}
+
+function getActivityHistory(membType, destinyMembershipId, characterId, secId) {
+  var fetchUrl =
+    "https://www.bungie.net/Platform/Destiny2/" +
+    membType +
+    "/Account/" +
+    destinyMembershipId +
+    "/Character/" +
+    characterId +
+    "/Stats/Activities/?count=2&mode=32";
+
+  fetch(fetchUrl, settings)
+    .then(r => {
+      return r.json();
+    })
+    .then(res => {
+      console.log(res);
+
+      res.Response.activities.forEach(element =>
+        getPostGameCarnageReport(element.activityDetails.instanceId, secId)
+      );
+    });
+}
+
+function getPostGameCarnageReport(activityId, secId) {
+  var fetchUrl =
+    "https://www.bungie.net/Platform/Destiny2/Stats/PostGameCarnageReport/" +
+    activityId +
+    "/";
+
+  fetch(fetchUrl, settings)
+    .then(r => {
+      return r.json();
+    })
+    .then(res => {
+      console.log(res);
+
+      //getting the team were the frist name is, and saving it in the standing variable
+      var standing = 0;
+      res.Response.entries.forEach(e => {
+        if (e.player.destinyUserInfo.displayName == "Bax") {
+          standing = e.standing;
+        }
+      });
+
+      //foreach person in the match create a link and do the check if they played togheter
+      res.Response.entries.forEach(element => {
+        createLiAndA(
+          element.player.destinyUserInfo.displayName,
+          "https://www.google.it/",
+          element.standing == standing
+        );
+        //checking if is in the enemy team
+        if (element.standing != standing) {
+          //checking if someone in the other team is the second name given
+          if (element.player.destinyUserInfo.membershipId == secId) {
+            console.log("played");
+            createEntryMatch(element.player.destinyUserInfo.membershipId);
+          }
+        }
+      });
+    });
+}
+
+function showStatsOfMatch(obj) {
+  console.log(obj);
+}
